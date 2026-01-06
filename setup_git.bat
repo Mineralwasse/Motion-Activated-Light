681ff874296168b72ab83482d6f33f0472da4fc7
Mineralwasse <mineralwasserxx25@yahoo.de>
2026-01-06 10:48:56 +0100

    Initial commit: Motion Activated Light Blueprint

--- setup_git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Konfiguriere Git Credential Helper...
-"C:\Program Files\Git\cmd\git.exe" config --global credential.helper store
-echo Initialer Fetch...
-"C:\Program Files\Git\cmd\git.exe" fetch origin
-echo Git Setup abgeschlossen.
-pause
